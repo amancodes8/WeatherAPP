38a2babc1ca09f0501926f7df0dde10fe1c448c6
Aman <am123aman@gmail.com>
2024-07-03 20:56:26 +0530

    Done

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const temperatureElement = document.getElementById('temperature');
 const conditionElement = document.getElementById('condition');
 const rain = document.getElementById('rain');
 const hideOnClick = document.getElementById('hideOnClick');
+const WeatherCard = document.getElementById('WeatherCard');
+
+document.createAttribute('p').innerHTML = "Error while getting countr"
 
 document.getElementById('btn').addEventListener('click', function () {
     const cityName = cityInput.value;
@@ -17,31 +20,40 @@ document.getElementById('btn').addEventListener('click', function () {
         }
     };
 
-    async function fetchWeather() {
-        try {
-            const response = await fetch(url, options);
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
+    
+    if(cityName){
+        async function fetchWeather() {
+            try {
+                const response = await fetch(url, options);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const result = await response.json();
+                console.log(result);
+    
+                // Update the weather information in the UI
+                if(cityName === "Delhi" || "delhi"){
+                    result.location.region = "India"
+                }
+                locationElement.textContent = `${result.location.name}, ${result.location.region}, ${result.location.country}`;
+                temperatureElement.textContent = `Temperature: ${result.current.temp_c} °C Feels like: ${result.current.feelslike_c}`;
+                conditionElement.textContent = `Condition: ${result.current.condition.text}`;
+                rain.textContent = `Humidity: ${result.current.humidity}
+                                    Cloud: ${result.current.cloud},
+                                    Wind: ${result.current.wind_kph}, ${result.current.wind_dir}
+                                    Heat Index: ${result.current.heatindex_c}`
+                icon.src = result.current.condition.icon;
+                icon.classList.remove('hidden');
+            } catch (error) {
+                console.log('Error fetching weather data:', error);
             }
-            const result = await response.json();
-            console.log(result);
-
-            // Update the weather information in the UI
-            locationElement.textContent = `${result.location.name}, ${result.location.region}, ${result.location.country}`;
-            temperatureElement.textContent = `Temperature: ${result.current.temp_c} °C Feels like: ${result.current.feelslike_c}`;
-            conditionElement.textContent = `Condition: ${result.current.condition.text}`;
-            rain.textContent = `Humidity: ${result.current.humidity}
-                                Cloud: ${result.current.cloud}
-                                Wind: ${result.current.wind_kph} ${result.current.wind_dir}
-                                Heat Index: ${result.current.heatindex_c}`
-            icon.src = result.current.condition.icon;
-            icon.classList.remove('hidden');
-        } catch (error) {
-            console.log('Error fetching weather data:', error);
         }
+    
+        fetchWeather();
+    }else{
+        WeatherCard.innerHTML = "Please Enter a valid city"
     }
 
-    fetchWeather();
 
     cityInput.value = null;
 });
